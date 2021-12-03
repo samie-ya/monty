2f21984dc4c54ce6270ce70611dcd5082f224dbb
Samie <samrasolomon40@gmail.com>
2021-12-03 00:02:43 +0000

    Modified a file

--- function_3.c
+++ function_3.c
@@ -50,7 +50,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (head->n < 0 || head->n > 128)
+	if (head->n < 0 || head->n > 256)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n",
 line_number);
@@ -76,7 +76,7 @@ void pstr(stack_t **stack, unsigned int __attribute__((__unused__))line_number)
 
 	while (*stack != NULL)
 	{
-		if ((head->n <= 0 || head->n > 128))
+		if ((head->n <= 0 || head->n > 256))
 			break;
 		dprintf(STDOUT_FILENO, "%c", head->n);
 	head = head->next;
